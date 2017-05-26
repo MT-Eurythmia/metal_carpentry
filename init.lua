c47dc3615c63862fc2e295aaf580e7af6ca8f296
Grossam <lejimi1972@free.fr>
2017-05-26 16:15:16 +0200

    Correctio bug…

--- init.lua
+++ init.lua
@@ -1,17 +1,19 @@
 minetest.register_node('metal_carpentry:rusty_iron_block', {
+	description = 'Rusty iron block',
 	tiles = {'metal_carpentry_rusty_iron.png'},
-	group = {oddly_breakable_by_hand=2},
+	groups = {cracky=1, level=2}
 })
 
 minetest.register_craft ({
-	type='shapeless',
-	output='metal_carpentry:rusty_iron_block 1',
-	recipe={'default:dirt', 'default:steelblock'}
+	type = 'shapeless',
+	output = 'metal_carpentry:rusty_iron_block 1',
+	recipe = {'default:dirt', 'default:steelblock'}
 })
 
 minetest.register_node('metal_carpentry:old_bronze_block', {
+	description = 'Old bronze block',
 	tiles = {'metal_carpentry_old_bronze.png'},
-	group = {oddly_breakable_by_hand=2}
+	groups = {cracky=1, level=2}
 })
 
 minetest.register_craft ({
