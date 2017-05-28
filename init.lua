55cceb35239736d86a11befe142c2ec0397456c6
Grossam <lejimi1972@free.fr>
2017-05-28 11:51:09 +0200

    Ajout d'un node ave mesh

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+--/!\ Lua n'accepte pas le paramètres nommés !
+--Cette fonction particulière attend deux paramètres :
+--une chaîne de caractères et un tableau qui est sans doute "parsé" par la suite.
 minetest.register_node('metal_carpentry:rusty_iron_block', {
 	description = 'Rusty iron block',
 	tiles = {'metal_carpentry_rusty_iron.png'},
@@ -24,6 +27,36 @@ minetest.register_craft({
 	type='shapeless',
 	output='metal_carpentry:old_bronze_block 1',
 	recipe={'default:dirt', 'default:bronzeblock'},
+})
+
+minetest.register_node('metal_carpentry:ubeam_steel', {
+	description = 'Steel U beam',
+	drawtype = 'mesh',
+	mesh = 'metal_carpentry_ubeam.obj',
+	tiles = {'default_steel_block.png'},
+	groups = {cracky=1, level=2},
+	is_ground_content = false,
+	sounds = default.node_sound_metal_defaults(),
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {
+			--right, bottom, back, left, top, front (origine au centre du node)
+			{0.25, 0.4, 0.5, -0.25, 0.5, -0.5},
+			{0.25, -0.5, 0.5, -0.25, -0.4, -0.5},
+			{-0.15, -0.5, 05, -0.25, 0.5, -0.5},
+		},
+	},
+	collision_box = {
+		type = 'fixed',
+		fixed = {
+			--right, bottom, back, left, top, front (origine au centre du node)
+			{0.25, 0.4, 0.5, -0.25, 0.5, -0.5},
+			{0.25, -0.5, 0.5, -0.25, -0.4, -0.5},
+			{-0.15, -0.5, 05, -0.25, 0.5, -0.5},
+		},
+	},
 })
 
 --function stairs.register_stair_and_slab(subname, recipeitem,
